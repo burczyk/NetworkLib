1d2f2bac3b94852d66feabf8873a15c44d709f46
Kamil Burczyk <kamil.burczyk@sigmapoint.pl>
2014-02-25 19:51:20 +0100

    Changes for 2nd part of tutorial

--- NetworkLib/NetworkLib.h
+++ NetworkLib/NetworkLib.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-#import "AFNetworking.h"
+@class AFHTTPRequestOperationManager;
 
 @interface NetworkLib : NSObject
 
--- NetworkLib/NetworkLib.m
+++ NetworkLib/NetworkLib.m
@@ -7,6 +7,7 @@
 //
 
 #import "NetworkLib.h"
+#import "AFNetworking.h"
 
 @implementation NetworkLib
 
